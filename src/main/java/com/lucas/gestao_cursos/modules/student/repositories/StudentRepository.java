05ca875a4ca8baae9b27379d8bebd7c10ed06594
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-12 19:26:26 -0300

    adição metodo Optinal no repository

--- src/main/java/com/lucas/gestao_cursos/modules/student/repositories/StudentRepository.java
+++ src/main/java/com/lucas/gestao_cursos/modules/student/repositories/StudentRepository.java
@@ -1,5 +1,6 @@
 package com.lucas.gestao_cursos.modules.student.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lucas.gestao_cursos.modules.student.entities.StudentEntity;
 
 public interface StudentRepository extends JpaRepository<StudentEntity, UUID>{
-    
+    Optional<StudentEntity> findByUsernameOrEmail(String username, String email);
 }
