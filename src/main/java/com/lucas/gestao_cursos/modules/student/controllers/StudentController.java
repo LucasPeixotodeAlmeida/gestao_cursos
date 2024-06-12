67a859c482807d5246319972cb6b2aa54b327838
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-12 17:55:18 -0300

    utilizando o StudentRepository para criar um cadastro via StudentController

--- src/main/java/com/lucas/gestao_cursos/modules/student/controllers/StudentController.java
+++ src/main/java/com/lucas/gestao_cursos/modules/student/controllers/StudentController.java
@@ -1,21 +1,25 @@
 package com.lucas.gestao_cursos.modules.student.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.gestao_cursos.modules.student.entities.StudentEntity;
+import com.lucas.gestao_cursos.modules.student.repositories.StudentRepository;
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
     
     @PostMapping("/")
-    public void createStudent(@Valid @RequestBody StudentEntity studentEntity){
-        System.out.println("estudante:");
-        System.out.println(studentEntity.getEmail());
+    public StudentEntity createStudent(@Valid @RequestBody StudentEntity studentEntity){
+        return studentRepository.save(studentEntity);
     }
 }
