12e08d290d422f1708face163ae345cb623b3572
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-14 19:07:58 -0300

    criação do authInstructorController

---
+++ src/main/java/com/lucas/gestao_cursos/modules/instructors/controllers/AuthInstructorController.java
@@ -0,0 +1,32 @@
+package com.lucas.gestao_cursos.modules.instructors.controllers;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucas.gestao_cursos.modules.instructors.dto.AuthInstructorDTO;
+import com.lucas.gestao_cursos.modules.instructors.services.AuthInstructorService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthInstructorController {
+    
+    @Autowired
+    private AuthInstructorService authInstructorService;
+
+    @PostMapping("/instructor")
+    public ResponseEntity<Object> create(@RequestBody AuthInstructorDTO authInstructorDTO) throws AuthenticationException{
+        try {
+            var result = this.authInstructorService.execute(authInstructorDTO);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
