7c7ab5071d3867cb170832cd41586c06192674c0
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-14 17:35:42 -0300

    gravando senha StudentEntity criptografada

--- src/main/java/com/lucas/gestao_cursos/modules/student/services/CreateStudentService.java
+++ src/main/java/com/lucas/gestao_cursos/modules/student/services/CreateStudentService.java
@@ -1,6 +1,7 @@
 package com.lucas.gestao_cursos.modules.student.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.lucas.gestao_cursos.exceptions.UserFoundException;
@@ -13,11 +14,16 @@ public class CreateStudentService {
     @Autowired
     private StudentRepository studentRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public StudentEntity execute(StudentEntity studentEntity){
         this.studentRepository.findByUsernameOrEmail(studentEntity.getUsername(), studentEntity.getEmail())
         .ifPresent((user) ->{
             throw new UserFoundException();
         });
+        var password = passwordEncoder.encode(studentEntity.getPassword());
+        studentEntity.setPassword(password);
         return studentRepository.save(studentEntity);
     }
     
