bc17fc31bd6bfdc613c3bb0bd375f73700b12991
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-14 17:33:20 -0300

    gravando senha instructorEntity criptografada

--- src/main/java/com/lucas/gestao_cursos/modules/instructors/services/CreateInstructorService.java
+++ src/main/java/com/lucas/gestao_cursos/modules/instructors/services/CreateInstructorService.java
@@ -1,6 +1,7 @@
 package com.lucas.gestao_cursos.modules.instructors.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.lucas.gestao_cursos.exceptions.UserFoundException;
@@ -12,13 +13,17 @@ public class CreateInstructorService {
 
     @Autowired
     private InstructorRepository instructorRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     
     public InstructorEntity execute(InstructorEntity instructorEntity){
         this.instructorRepository.findByUsernameOrEmail(instructorEntity.getUsername(), instructorEntity.getEmail())
         .ifPresent((user) ->{
             throw new UserFoundException();
         });
-        ;
+        var password = passwordEncoder.encode(instructorEntity.getPassword());
+        instructorEntity.setPassword(password);
         return this.instructorRepository.save(instructorEntity);
     }
 }
