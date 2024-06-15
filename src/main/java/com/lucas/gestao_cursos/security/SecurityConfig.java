548084905f75e84ae2974e446635b79fe92dba79
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-15 18:44:55 -0300

    recuperando o JWT

--- src/main/java/com/lucas/gestao_cursos/security/SecurityConfig.java
+++ src/main/java/com/lucas/gestao_cursos/security/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.lucas.gestao_cursos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
@@ -17,7 +23,9 @@ public class SecurityConfig {
                     .requestMatchers("/instructor/").permitAll()
                     .requestMatchers("/auth/instructor").permitAll();
                 auth.anyRequest().authenticated();
-            });
+            })
+            .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+            ;
             return http.build();
     }
 
