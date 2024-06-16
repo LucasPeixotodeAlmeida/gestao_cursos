d8d4f754247d35bf850c48399df1564aa8937a82
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-16 16:37:58 -0300

    criação do authStudentController

---
+++ src/main/java/com/lucas/gestao_cursos/modules/student/controllers/AuthStudentController.java
@@ -0,0 +1,30 @@
+package com.lucas.gestao_cursos.modules.student.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucas.gestao_cursos.modules.student.dto.AuthStudentRequestDTO;
+import com.lucas.gestao_cursos.modules.student.services.AuthStudentService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthStudentController {
+
+    @Autowired
+    private AuthStudentService authStudentService;
+
+    @PostMapping("/student")
+    public ResponseEntity<Object> auth(@RequestBody AuthStudentRequestDTO authStudentRequestDTO){
+        try {
+            var token = this.authStudentService.execute(authStudentRequestDTO);
+            return ResponseEntity.ok().body(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
