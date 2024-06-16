a35260b73efaf8b77aa51b3e5c1a38907a53670b
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-16 14:33:07 -0300

    refatorando CourseController

--- src/main/java/com/lucas/gestao_cursos/modules/instructors/controllers/CourseController.java
+++ src/main/java/com/lucas/gestao_cursos/modules/instructors/controllers/CourseController.java
@@ -1,14 +1,18 @@
 package com.lucas.gestao_cursos.modules.instructors.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucas.gestao_cursos.modules.instructors.dto.CreateCourseDTO;
 import com.lucas.gestao_cursos.modules.instructors.entities.CourseEntity;
 import com.lucas.gestao_cursos.modules.instructors.services.CreateCourseService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -19,7 +23,18 @@ public class CourseController {
     private CreateCourseService createCourseService;
     
     @PostMapping("/")
-    public CourseEntity createCourse(@Valid @RequestBody CourseEntity courseEntity){
-           return this.createCourseService.execute(courseEntity);
+    public CourseEntity createCourse(@Valid @RequestBody CreateCourseDTO createCourseDTO, HttpServletRequest request){
+        var instructorId = request.getAttribute("instructor_id");
+
+        var courseEntity = CourseEntity.builder()
+            .active(createCourseDTO.getActive())
+            .category(createCourseDTO.getCategory())
+            .description(createCourseDTO.getDescription())
+            .name(createCourseDTO.getName())
+            .instructorId(UUID.fromString(instructorId.toString()))
+            .build();
+
+
+        return this.createCourseService.execute(courseEntity);
     }
 }
